439cdd0bfce9376bc0f4d5a674647aa396b96db4
Santiago Zappa <santiagozappab@gmail.com>
2026-03-07 16:30:06 -0500

    wasclonee

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- Añade esto
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <--- Añade esto antes o después de react()
-  ],
+  plugins: [react()],
 })
