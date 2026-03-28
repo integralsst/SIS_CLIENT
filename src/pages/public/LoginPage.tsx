35d1745e7b849e3a3ac8718d40fa797e293692a4
Juan Zappa <juanzappa@MacBook-Air-de-Juan.local>
2026-03-28 15:07:03 -0500

    Adminupgrade

--- src/pages/public/LoginPage.tsx
+++ src/pages/public/LoginPage.tsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useAuth } from '../../context/AuthContext';
 
-import Logo from '../assets/logosis.webp'; 
+import Logo from '../../assets/logosis.webp'; 
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
